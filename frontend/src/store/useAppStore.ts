f7c050b599a283acc0fcbd0090d3ac43f80fae6e
jhh7243-cpu <jhh7243@gmail.com>
2025-11-20 17:14:09 +0900

    시험

--- frontend/src/store/useAppStore.ts
+++ frontend/src/store/useAppStore.ts
@@ -1,21 +1,64 @@
 import { create } from "zustand";
 import { AppStore } from "./types";
+import { createUiSlice } from "./slices/uiSlice";
+import { createSoccerSlice } from "./slices/soccerSlice";
+import { createDiarySlice } from "./slices/diarySlice";
+import { createCalendarSlice } from "./slices/calendarSlice";
+import { createAccountSlice } from "./slices/accountSlice";
+import { createCultureSlice } from "./slices/cultureSlice";
+import { createHealthSlice } from "./slices/healthSlice";
+import { createPathSlice } from "./slices/pathSlice";
+import { createInteractionSlice } from "./slices/interactionSlice";
+import { createAvatarSlice } from "./slices/avatarSlice";
 
 /**
  * 단일 Zustand 스토어 (슬라이스 패턴 적용)
  * 
- * 12개 서비스 (AI 에이전트 5개 + MS 7개)를 위한 확장 가능한 구조
+ * 카테고리별로 분리된 슬라이스 구조
  * 
  * 사용법:
- * - 각 서비스별로 독립적인 슬라이스 생성
+ * - 각 카테고리별로 독립적인 슬라이스 생성
  * - 모든 슬라이스를 여기에 통합
  * - 컴포넌트에서 선택적 구독으로 사용
  * 
  * 예시:
- * const data = useAppStore((state) => state.agent1.data);
- * const fetchData = useAppStore((state) => state.agent1.fetchData);
+ * // UI 상태
+ * const darkMode = useAppStore((state) => state.ui.darkMode);
+ * const toggleDarkMode = useAppStore((state) => state.ui.toggleDarkMode);
+ * 
+ * // 일기 카테고리
+ * const diaryView = useAppStore((state) => state.diary.diaryView);
+ * const setDiaryView = useAppStore((state) => state.diary.setDiaryView);
+ * 
+ * // 캘린더 카테고리
+ * const events = useAppStore((state) => state.calendar.events);
+ * const addEvent = useAppStore((state) => state.calendar.addEvent);
+ * 
+ * // 인터랙션
+ * const inputText = useAppStore((state) => state.interaction.inputText);
+ * const addInteraction = useAppStore((state) => state.interaction.addInteraction);
  */
 export const useAppStore = create<AppStore>()((...a) => ({
+  // 공통 UI 상태 슬라이스
+  ui: createUiSlice(...a),
+  
+  // 인터랙션 & 프롬프트 슬라이스
+  interaction: createInteractionSlice(...a),
+  
+  // 아바타 모드 슬라이스
+  avatar: createAvatarSlice(...a),
+  
+  // 카테고리별 슬라이스
+  diary: createDiarySlice(...a),
+  calendar: createCalendarSlice(...a),
+  account: createAccountSlice(...a),
+  culture: createCultureSlice(...a),
+  health: createHealthSlice(...a),
+  path: createPathSlice(...a),
+  
+  // 서비스 슬라이스
+  soccer: createSoccerSlice(...a),
+  
   // TODO: AI 에이전트 슬라이스들 (5개)
   // agent1: createAgent1Slice(...a),
   // agent2: createAgent2Slice(...a),
@@ -23,12 +66,11 @@ export const useAppStore = create<AppStore>()((...a) => ({
   // agent4: createAgent4Slice(...a),
   // agent5: createAgent5Slice(...a),
   
-  // TODO: 마이크로서비스 슬라이스들 (7개)
+  // TODO: 마이크로서비스 슬라이스들 (나머지 6개)
   // service1: createService1Slice(...a),
   // service2: createService2Slice(...a),
   // service3: createService3Slice(...a),
   // service4: createService4Slice(...a),
   // service5: createService5Slice(...a),
   // service6: createService6Slice(...a),
-  // service7: createService7Slice(...a),
 }));
